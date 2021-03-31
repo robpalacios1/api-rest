dc540a514e0ef1037357057e35be3ec83a8ab995
roberto <robpalacios1@hotmail.com>
2021-03-30 20:19:45 -0500

    add section header

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const burger = document.querySelector(".js-burger-button")
+const burgerClose = document.querySelector(".js-burger-close")
+const header = document.querySelector(".js-header")
+const stickyHeader = document.querySelector(".js-sticky")
+
+//function for burger button click
+burger.addEventListener('click', () => {
+  header.classList.toggle("header--active");
+})
+
+burgerClose.addEventListener('click', () => {
+  header.classList.toggle("header--active");
+})
+
+function headerResize() {
+
+  if (screen.width >= 1024) {
+    const headerActiveElements = document.getElementsByClassName('header--active');
+    if (headerActiveElements.length > 0) {
+      header.classList.remove("header--active");
+    }
+  }
+}
+
+window.addEventListener('resize',headerResize);
+
+
+// function for header sticky
+
+window.onscroll = function() {myFunction()};
+
+const sticky = stickyHeader.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    stickyHeader.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
