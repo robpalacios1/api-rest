a5cc61ea9c26a569439d5eaa05b808a0d8511a48
roberto <robpalacios1@hotmail.com>
2021-04-05 11:19:49 -0500

    implement funtion to show blog

--- js/api.js
+++ js/api.js
@@ -3,8 +3,8 @@ let cardContent = document.querySelector('.js-card-container');
 let searchContent = document.querySelector('.js-search');
 let searchButton = document.querySelector('.js-search-button');
 let searchCounter = document.querySelector('.js-search-counter');
+let myDictionary = {}
 let cardsArray = '<div class="card__content">';
-
 
 
 searchButton.addEventListener("click", () => {
@@ -19,133 +19,46 @@ searchButton.addEventListener("click", () => {
 
     let allCards = '<div class="card__content">';
     cardsArray = allCards;
-    console.log(searchContent.value);
     cards.forEach(card => {
 
       // filter search.
       if(card.sipnosis.includes(searchContent.value)) {
-
+        
         if(counter <= 5) {
           allCards += drawCard(card);
         }
 
-        cardsArray += `
-            <div class="card__main-card">
-              <div class="card__main-image">
-                <div class="card__image">
-                  <img src="${card.image}" alt="${card.image}"/>
-                  <div class="card__price">
-                    <a href="#" class="card__price-link"
-                      ><button class="card__price-button">${card.price}</button></a
-                    >
-                  </div>
-                  <div class="card__favorite">
-                    <a href="#"><i class="fas fa-star"></i></a>
-                  </div>
-                </div>
-              </div>
-    
-              <div class="card__bottom">
-                <div class="card__title">
-                  <h2>${card.title}</h2>
-                </div>
-    
-                <div class="card__sipnosis">
-                  <span
-                    >${card.sipnosis}...</span
-                  >
-                </div>
-    
-                <hr class="card__hr" />
-    
-                <div class="card__main-footer">
-                  <div class="card__avatar">
-                    <img src="${card.avatar}" alt="${card.avatar}" />
-                  </div>
-    
-                  <div class="card__name">
-                    <h3>${card.name}</h3>
-                  </div>
-    
-                  <div class="card__date">
-                    <h3>${card.date}</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-          `;
+        cardsArray += drawCard(card);
 
+        myDictionary[`n${card.id}`] = card;
         counter += 1;
+
       } else {
-        searchCounter.textContent = 'Do Not Find Any result';
+        searchCounter.textContent = 'no results';
       };
     });
 
     //show card not-found.
     if(counter == 0) {
-      cardContent.innerHTML = `
-      <div class="card__not-found">
-      <div class="card__left">
-        <div class="card__title-image">
-          <h2>Uh oh.</h2>
-        </div>
-    
-        <div class="card__subtitle">
-          <span
-            >We ran into an issue, but don't worry, <br />
-            we'll take care of it for sure</span
-          >
-        </div>
-        <div class="card__button-align">
-          <button class="card__button-safety">Back to safety</button>
-        </div>
-      </div>
-    
-      <div class="card__right">
-        <div class="card__image-not">
-          <img src="./assets/not-found.png" alt="not found" />
-        </div>
-      </div>
-    </div>
-      `;
-      return 0;
+      cardContent.innerHTML = drawNotFound();
+      return;
     }
 
     allCards += '</div>';
-
     cardsArray += '</div>';
+
+    cardContent.innerHTML = allCards;
+    searchCounter.textContent = `${counter} RESULTS`;
 
     // show button to load more.
     if(counter > 6) {
-      allCards += `
-        <div class="card__load">
-        <button class="card__load-more js-load-more">
-          Load More
-          <svg
-            class="card__arrow"
-            width="12"
-            height="12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M6.526.22a.75.75 0 10-1.06 1.06l3.97 3.97H.75a.75.75 0 000 1.5h8.685l-3.97 3.97a.75.75 0 101.061 1.06l5.207-5.206a.748.748 0 000-1.148L6.526.22z"
-              fill="#335EEA"
-            />
-          </svg>
-        </button>
-      </div>
-      `;
-    }
-
-    cardContent.innerHTML = allCards
-    searchCounter.textContent = `${counter} RESULTS`;
-
-    if (counter > 6) {
+      allCards += showButton();
+      cardContent.innerHTML = allCards;
       loadAll();
     }
+
+    showBlog();
+
   })
   .catch(error => {
     console.log(error)
@@ -158,24 +71,32 @@ function loadAll() {
 
   loadMore.addEventListener('click', () => {
     cardContent.innerHTML = cardsArray;
+    showBlog();
   });
+
 }
 
 // function lo load blog when click card.
 function showBlog() {
-  document.querySelector('.js-card-desktop').forEach(card => {
-    card.addEventListener('click', (e) => {
-      e.preventDefault();
-      cardContent.innerHTML
-      return `
 
-      `
+  console.log('entró');
+
+  document.querySelectorAll('.js-card-desktop').forEach(card => {
+
+    card.addEventListener('click', (e)=>{
+
+      e.preventDefault();
+      
+      cardContent.innerHTML = drawBlog(myDictionary[`n${e.target.dataset.id}`]);
     })
-  });
+  })
+
+
+
 }
 
 // function to draw the blog
-function drawBlog({id, price, image, title, sipnosis, avatar, name, date}) {
+function drawBlog({image, title, name, sipnosis, date}) {
   return `
   <div class="card__desktop">
   <div class="card__title-desktop">
@@ -197,7 +118,7 @@ function drawBlog({id, price, image, title, sipnosis, avatar, name, date}) {
       </div>
 
       <div class="card__name-desktop">
-        <h3>Alive Cooper</h3>
+        <h3>${name}</h3>
         <div class="card__published-desktop">
           <h4>Published on ${date}, 2019</h4>
         </div>
@@ -252,14 +173,12 @@ function drawBlog({id, price, image, title, sipnosis, avatar, name, date}) {
 function drawCard({id, price, image, title, sipnosis, avatar, name, date}) {
   return `
     <div class="card__main-card">
-      <a class="card__link js-card-desktop" href="#" data-id="${id}">
+      <a class="card__link-wrap js-card-desktop" href="#" data-id="${id}"></a>
         <div class="card__main-image">
           <div class="card__image">
             <img src="${image}" alt="${image}"/>
               <div class="card__price">
-                <a href="#" class="card__price-link"
-                    ><button class="card__price-button">${price}</button></a
-                >
+                <a href="#" class="card__price-link"><button class="card__price-button">${price}</button></a>
                 </div>
                 <div class="card__favorite">
                   <a href="#"><i class="fas fa-star"></i></a>
@@ -271,31 +190,82 @@ function drawCard({id, price, image, title, sipnosis, avatar, name, date}) {
             <div class="card__title">
               <h2>${title}</h2>
             </div>
-    
+
             <div class="card__sipnosis">
-              <span
-                >${sipnosis}...</span
-                >
+              <span>${sipnosis}...</span>
             </div>
-    
+
             <hr class="card__hr" />
-    
+
             <div class="card__main-footer">
               <div class="card__avatar">
                 <img src="${avatar}" alt="${avatar}" />
               </div>
-    
+
               <div class="card__name">
                 <h3>${name}</h3>
               </div>
-    
+
               <div class="card__date">
                 <h3>${date}</h3>
               </div>
             </div>
           </div>
-        </a>
+
       </div>
   `
 }
 
+// function to draw the card not found
+function drawNotFound() {
+ return `
+ <div class="card__not-found">
+ <div class="card__left">
+   <div class="card__title-image">
+     <h2>Uh oh.</h2>
+   </div>
+
+   <div class="card__subtitle">
+     <span
+       >We ran into an issue, but don't worry, <br />
+       we'll take care of it for sure</span
+     >
+   </div>
+   <div class="card__button-align">
+     <button class="card__button-safety">Back to safety</button>
+   </div>
+ </div>
+
+ <div class="card__right">
+   <div class="card__image-not">
+     <img src="./assets/not-found.png" alt="not found" />
+   </div>
+ </div>
+</div>
+ `;
+}
+
+//function to show the button
+function showButton() {
+  return `
+  <div class="card__load">
+  <button class="card__load-more js-load-more">
+    Load More
+    <svg
+      class="card__arrow"
+      width="12"
+      height="12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M6.526.22a.75.75 0 10-1.06 1.06l3.97 3.97H.75a.75.75 0 000 1.5h8.685l-3.97 3.97a.75.75 0 101.061 1.06l5.207-5.206a.748.748 0 000-1.148L6.526.22z"
+        fill="#335EEA"
+      />
+    </svg>
+  </button>
+</div>
+`;
+}
